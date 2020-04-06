d477eb9a710479a48cf9329b12185883c75d6daf
Pierce T <ptrotman22@gmail.com>
2020-04-06 06:28:47 -0400

    Hit System Created and Testing
    Hit glitch is not adding to the grid.

--- src/BattleshipDriver.java
+++ src/BattleshipDriver.java
@@ -13,10 +13,20 @@ public class BattleshipDriver {
             int[] location = new int[2];
             String[][] grid = new String[10][10];
             String[][] grid2 = new String[10][10];
+            String[][] hitGrid = new String[10][10];
+            String[][] hitGrid2 = new String[10][10];
             int size = 0;
             Sea sea = new Sea();
+            // Player 1 Objects
             Carrier p1Carrier = new Carrier();
+            Battleship p1Battleship = new Battleship();
+            Destroyer p1Destroyer = new Destroyer();
+            PatrolBoat p1PatrolBoat = new PatrolBoat();
+            // Player 2 Objects
             Carrier p2Carrier = new Carrier();
+            Battleship p2Battleship = new Battleship();
+            Destroyer p2Destroyer = new Destroyer();
+            PatrolBoat p2PatrolBoat = new PatrolBoat();
             // Game Opening
             System.out.println("Battleship Multiplayer");
             System.out.println("Enter Player 1 name: ");
@@ -26,7 +36,7 @@ public class BattleshipDriver {
 
             // Place Ships on Board
             sea.PrintGrid();
-            System.out.println("Player 1 place your BattleShips");
+            System.out.println(player1 +" place your BattleShips: Carrier(5 places)");
             System.out.println("Please choose first coordinate:");
             int input = new Scanner(System.in).nextInt();
             location[0] = input;
@@ -38,8 +48,143 @@ public class BattleshipDriver {
             size = p1Carrier.Size();
             grid = sea.Grid(true, player1, location, grid, size);
 
-            
+            grid = PlaceShips(player1, grid, location, p1Battleship.Size());
+            grid = PlaceShips(player1, grid, location, p1Destroyer.Size());
+            grid = PlaceShips(player1, grid, location, p1PatrolBoat.Size());
+
+            sea.PrintGrid();
+            grid2 = PlaceShips(player2, grid2, location, p2Carrier.Size());
+            grid2 = PlaceShips(player2, grid2, location, p2Battleship.Size());
+            grid2 = PlaceShips(player2, grid2, location, p2Destroyer.Size());
+            grid2 = PlaceShips(player2, grid2, location, p2PatrolBoat.Size());
+
+            System.out.println("");
+
+            // Hit System
+            hitGrid = sea.FillGrid();
+            hitGrid2 = sea.FillGrid();
+            boolean endHit = false;
+            int win1 = 0;
+            int win2 = 0;
+            do {
+                // Player 1 hit Game
+                sea.PrintGrid(hitGrid);
+                System.out.println(player1 + " find your opponents Battleships");
+                System.out.println("Please choose first coordinate:");
+                int input3 = new Scanner(System.in).nextInt();
+                location[0] = input3;
+                System.out.println("Please choose second coordinate:");
+                int input4 = new Scanner(System.in).nextInt();
+                location[1] = input4;
+                hitGrid = sea.HitGrid(grid2, hitGrid, location);
+                win1 = sea.WinCondition(hitGrid);
+                if (win1 == 14){
+                    System.out.println(player1 + " wins the Match");
+                    endHit = true;
+                    end = 1;
+                    break;
+                }
+
+                System.out.println("");
+
+                // Player 2 hit Game
+                sea.PrintGrid(hitGrid2);
+                System.out.println(player2 + " find your opponents Battleships");
+                System.out.println("Please choose first coordinate:");
+                int input5 = new Scanner(System.in).nextInt();
+                location[0] = input5;
+                System.out.println("Please choose second coordinate:");
+                int input6 = new Scanner(System.in).nextInt();
+                location[1] = input6;
+                hitGrid2 = sea.HitGrid(grid, hitGrid2, location);
+                win2 = sea.WinCondition(hitGrid2);
+                if (win2 == 14){
+                    System.out.println(player2 + " wins the Match");
+                    endHit = true;
+                    end = 1;
+                    break;
+                }
+
+                System.out.println("");
+
+            } while (endHit = false);
+
         }while(end != 1);
+
+        System.out.println("Thank you for playing");
+
+    }
+    public static String[][] PlaceShips(String player, String[][] currentGrid, int[] location, int size){
+        String[][] grid = new String[10][10];
+        grid = currentGrid.clone();
+        Sea sea = new Sea();
+        // Player Objects
+        Carrier carrier = new Carrier();
+        Battleship battleship = new Battleship();
+        Destroyer destroyer = new Destroyer();
+        PatrolBoat patrolBoat = new PatrolBoat();
+
+        String letter = "";
+        String shipName = "";
+        if (size == 5){
+            shipName = "Carrier";
+            System.out.println(player +" place your BattleShips: "+ shipName);
+            System.out.println("Please choose first coordinate:");
+            int input = new Scanner(System.in).nextInt();
+            location[0] = input;
+            System.out.println("Please choose second coordinate:");
+            int input1 = new Scanner(System.in).nextInt();
+            location[1] = input1;
+
+            location = carrier.Location(location, grid);
+            size = carrier.Size();
+            grid = sea.Grid(true, player, location, grid, size);
+        }
+        else if(size == 4){
+            shipName = "Battleship";
+            System.out.println(player +" place your BattleShips: "+ shipName);
+            System.out.println("Please choose first coordinate:");
+            int input = new Scanner(System.in).nextInt();
+            location[0] = input;
+            System.out.println("Please choose second coordinate:");
+            int input1 = new Scanner(System.in).nextInt();
+            location[1] = input1;
+
+            location = battleship.Location(location, grid);
+            size = battleship.Size();
+            grid = sea.Grid(false, player, location, grid, size);
+        }
+        else if(size == 3){
+            shipName = "Destroyer";
+            System.out.println(player +" place your BattleShips: "+ shipName);
+            System.out.println("Please choose first coordinate:");
+            int input = new Scanner(System.in).nextInt();
+            location[0] = input;
+            System.out.println("Please choose second coordinate:");
+            int input1 = new Scanner(System.in).nextInt();
+            location[1] = input1;
+
+            location = destroyer.Location(location, grid);
+            size = destroyer.Size();
+            grid = sea.Grid(false, player, location, grid, size);
+        }
+        else if(size == 2){
+            shipName = "Patrol Boat";
+            System.out.println(player +" place your BattleShips: "+ shipName);
+            System.out.println("Please choose first coordinate:");
+            int input = new Scanner(System.in).nextInt();
+            location[0] = input;
+            System.out.println("Please choose second coordinate:");
+            int input1 = new Scanner(System.in).nextInt();
+            location[1] = input1;
+
+            location = patrolBoat.Location(location, grid);
+            size = patrolBoat.Size();
+            grid = sea.Grid(false, player, location, grid, size);
+        }
+
+        // Return Grid
+        return grid;
 
     }
 }
--- src/main/java/Sea.java
+++ src/main/java/Sea.java
@@ -98,6 +98,7 @@ public class Sea {
             shipName = "Patrol Boat";
         }
 
+        // Move down or right
         System.out.println("Place "+ shipName +" ("+ size +" spaces) horizontally(Right) or vertically(Down) (h or v)?");
         String direction = new Scanner(System.in).nextLine();
 
@@ -173,22 +174,11 @@ public class Sea {
 
         return grid;
     }
-    public static void PrintGrid(){
+    public static void PrintGrid(String[][] currentGrid){
         String[][] grid = new String[10][10];
 
         //Fill grid
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                    grid[i][j] = " ~ ";
-            }
-        }
-            grid[0][0] = " X ";
-
-        for (int i = 1; i < grid.length; i++) {
-            grid[0][i] = String.valueOf(" "+ i +" ");
-            grid[i][0] = String.valueOf(" "+ i +" ");
-        }
+        grid = currentGrid.clone();
 
 
         // Print Map
@@ -242,6 +232,200 @@ public class Sea {
         }
         System.out.println();
 
+    }
+
+    public static void PrintGrid(){
+        String[][] grid = new String[10][10];
+
+        //Fill grid
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                    grid[i][j] = " ~ ";
+            }
+        }
+
+        grid[0][0] = " X ";
+
+        for (int i = 1; i < grid.length; i++) {
+            grid[0][i] = String.valueOf(" " + i + " ");
+            grid[i][0] = String.valueOf(" " + i + " ");
+        }
+
+
+
+        // Print Map
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[0][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[1][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[2][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[3][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[4][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[5][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[6][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[7][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[8][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(grid[9][i]);
+        }
+        System.out.println();
+
+    }
+
+    public static String[][] FillGrid(){
+        String[][] grid = new String[10][10];
+
+        // fill Grid
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                grid[i][j] = " ~ ";
+            }
+        }
+        grid[0][0] = " X ";
+
+        for (int i = 1; i < grid.length; i++) {
+            grid[0][i] = String.valueOf(" "+ i +" ");
+            grid[i][0] = String.valueOf(" "+ i +" ");
+        }
+
+        return grid;
+    }
+
+    public static String[][] HitGrid(String[][] currentGrid, String[][] currentHitGrid, int[] currentLocation){
+        String[][] grid = currentGrid.clone();
+        String[][] hitGrid = new String[10][10];
+
+        hitGrid = currentHitGrid.clone();
+
+        boolean filled = true;
+        boolean end = false;
+        while (end = false) {
+            while (filled = true) {
+                if (!grid[currentLocation[0]][currentLocation[1]].equalsIgnoreCase(" ~ ")) {
+                    if (hitGrid[currentLocation[0]][currentLocation[1]].equalsIgnoreCase(" h ") || hitGrid[currentLocation[0]][currentLocation[1]].equalsIgnoreCase(" m ")) {
+                        System.out.println("Sorry, You already picked this spot. Please try again");
+                        System.out.println("Please choose first coordinate:");
+                        int input = new Scanner(System.in).nextInt();
+                        currentLocation[0] = input;
+                        System.out.println("Please choose second coordinate:");
+                        int input1 = new Scanner(System.in).nextInt();
+                        currentLocation[1] = input1;
+                        break;
+                    } else {
+                        hitGrid[currentLocation[0]][currentLocation[1]] = " h ";
+                        filled = false;
+                        end = true;
+                    }
+                } else {
+                    hitGrid[currentLocation[0]][currentLocation[1]] = " m ";
+                    filled = false;
+                    end = true;
+                }
+            }
+        }
+
+        // Print Map
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[0][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[1][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[2][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[3][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[4][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[5][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[6][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[7][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[8][i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            System.out.print(hitGrid[9][i]);
+        }
+        System.out.println();
+
+        return hitGrid;
+
+    }
+
+    public static int WinCondition(String[][] currentHitGrid){
+        String[][] hitGrid = currentHitGrid.clone();
+        int count = 0;
+
+        for (int i = 0; i < hitGrid.length; i++) {
+            for (int j = 0; j < hitGrid[0].length; j++) {
+                if (hitGrid[i][j].equalsIgnoreCase(" h ")){
+                   count++;
+                }
+            }
+        }
+
+        return count;
     }
 
 }
